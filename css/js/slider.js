ffcfe5502147392bd1a977a3909360b7c643cea3
riomorita <ttc2350dl0011@edu.tech.ac.jp>
2025-04-25 18:44:31 +0900

    Add files via upload

---
+++ css/js/slider.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".slider").slick({
+        autoplay: true,
+        infinte: true,
+        slidesToShow: 3,
+        slidesTcScroll: 3,
+        prevArrow: '<div class="slick-prev"></div>'
+        nextArrow: '<div class="slick-next"></div>'
+
+    });
+});
